ce081dd662e2c0a75b9daee61c95638a87c6f223
Matt Ellis <matell@microsoft.com>
2017-03-16 19:20:35 -0700

    Support building on *nix
     - Add build.sh and other required scripts
     - Move off of nuproj for describing packages, instead use nupsecs
       directly so we can use the same files when packing with `dotnet`
     - Add projects to be able to build nupkg's with `dotnet` tooling
    
    There were some workarounds for issues with previous versions of the
    tooling that are no longer problems.

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+SCRIPT_ROOT="$(dirname "${BASH_SOURCE[0]}")"
+DOTNET_TOOL_ROOT="$SCRIPT_ROOT/obj/tools/dotnet"
+
+curl -L https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.sh | bash -s -- --version 1.0.0 --install-dir "$DOTNET_TOOL_ROOT"
+
+export PATH=$DOTNET_TOOL_ROOT:$PATH
+
+$SCRIPT_ROOT/tools/Build.sh
